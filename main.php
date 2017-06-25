c490661f4b2b222adbe4cc13732cb61a3963ce60
Bidhan sutradhar <bidhanvk@gmail.com>
2017-06-25 21:39:23 +0600

    text file name replace to main html

---
+++ main.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+	<div id="total_body">
+		<div class="header">
+		
+		</div>
+		<div class="menu">
+		
+		</div>
+	</div>
+</body>
+</html>
